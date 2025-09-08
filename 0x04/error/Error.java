e22a9a39db15a030c5ce063ce65ef0669ce26964
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-09-07 21:39:04 -0300

    1. Tratamento de erros

---
+++ 0x04/error/Error.java
@@ -0,0 +1,43 @@
+public class Error {
+
+
+    // Use Exceptions ao invés de retornar o código
+    //A
+    public User createUser(User user){
+        try {
+            loadUser(user.getId());
+            throw new UserExistsException(user.getId())
+        }
+        catch (UserNotFoundException exception){
+            // detail here
+        }
+        resolveGroup(user);
+        return userRepository.save(user);
+    }
+
+    // Exceções com contexto
+    // B
+    public void validate(String id){
+        if(ownerRepo.count(id) == 0){
+            throw new ValidationException("id: " + id + " não foi informado, deveria ser informado...")
+        }
+    }
+
+
+    // Não retorne null e não passe null
+    //B
+    public User createUser(User user){
+
+        try {
+            loadUser(user.getId());
+            throw new UserExistsException(user.getId())
+        }
+        catch (UserNotFoundException exception){
+            // detail here
+        }
+
+        resolveGroup(user);
+        return userRepository.save(user);
+    }
+
+}

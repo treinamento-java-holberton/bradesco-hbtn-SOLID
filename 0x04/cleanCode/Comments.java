dbf1a4e7b9a1a140cb172f031026cb4b27fb5e2e
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-09-07 21:36:46 -0300

    0. Comentarios

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,28 @@
+public class Comments {
+
+    // Copyright (C) 2022 by CleanCode.
+    // Lançado sob os termos da GNU General Public License versão 2 ou posterior
+
+
+    public Owner getTestcaseOwner(Testcase testcase){};
+
+
+    public Hours getHoursInQuarter(){};
+
+
+    // TODO Este método deve ser removido
+    public void check(){
+        return;
+    }
+
+
+    /**
+     * <p> este método publica resultados com base na data
+     * </p>
+     * @param data data a partir da qual os resultados devem ser publicados
+     * @retornar Lista de entidades de resultado
+     * @desde 16.0
+     */
+    public List<Result> publishResults(Date date) {
+    }
+}
